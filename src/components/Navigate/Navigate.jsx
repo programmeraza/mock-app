c5899822cedcfaca6039ec95117ed0049f203ee1
programmeraza <karimberdiyevaziz077@gmail.com>
2025-12-09 19:29:38 +0500

    feat: registr v3

--- src/components/Navigate/Navigate.jsx
+++ src/components/Navigate/Navigate.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import "./Navigate.scss"
+import { RiHome9Line } from "react-icons/ri";
+import { SlNotebook } from "react-icons/sl";
+import { RiSettings3Line } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
+
 
 const Navigate = () => {
   return (
     <>
-      
+      <div className="nav">
+        <div className="container">
+          <div className="nav__wrapper">
+            <Link className="nav__card">
+              <div className="nav__icon">
+                <RiHome9Line />
+              </div>
+              <p>Home</p>
+            </Link>
+            <Link className="nav__card">
+              <div className="nav__icon">
+                <SlNotebook />
+              </div>
+              <p>Mock Test</p>
+            </Link>
+            <Link className="nav__card">
+              <div className="nav__icon">
+                <RiSettings3Line />
+              </div>
+              <p>Settings</p>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Login from '../components/Login/Login'
 import Signup from '../components/Signup/Signup'
+import Dashboard from '../components/Dashboard/Dashboard'
+import Navigate from '../components/Navigate/Navigate'
 
 const HomePage = () => {
   return (
     <>
-    {/* <Login/> */}
-    {/* <Signup/> */}
-    Home Page
+    <Dashboard/>
+    <Navigate/>
     </>
   )
 }
